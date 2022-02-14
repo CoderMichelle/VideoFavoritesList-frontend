bbbacc08c858554965aef1b608bd798e30c7151f
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-13 22:19:05 -0800

    install browser-router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, root);
