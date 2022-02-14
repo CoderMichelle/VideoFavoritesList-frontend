f1414c32a9ff133fb8048189922a094779d6d073
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-13 22:10:20 -0800

    updated App

---
+++ src/components/AboutMichellePannosch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AboutMichellePannosch extends Component {
+  render() {
+    return (
+      <div>AboutMichellePannosch</div>
+    );
+  }
+}
+
+export default AboutMichellePannosch;
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello from App here is proof of life</div>
+    );
+  }
+}
+
+export default App;
