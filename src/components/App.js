2ef3a323b0b7a54189061c5b9cf027266883ff85
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-14 21:00:39 -0800

    form page done and styled, axios call to backend server implemented, changed Route pages in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,38 @@ import About from './About.js';
 import AboutMichellePannosch from './AboutMichellePannosch.js';
 import MoviesList from './MoviesList.js';
 
+import axios from 'axios';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+      error: false,
+      loading: false,
+      hasSearched: false,
+      resultsFromServer: [],
+      status: null,
+      movieResultsShowing: false
+    };
+  }
+
+  hoistInputFromMoviesForm = (inputfromform) => {
+    this.setState({ searchInput: inputfromform, hasSearched: true });
+    // console.log('we are inside of App and here is inputfromform', inputfromform);
+    this.apiCallTMDB(inputfromform);
+  };
+
+  apiCallTMDB = async (searchInput) => {
+    let resultsFromServer = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/movies?movieName=${searchInput}`);
+    console.log('hurray we contacted the server and here is what she said:', resultsFromServer);
+    this.setState({ resultsFromServer: resultsFromServer.data, status: resultsFromServer.status });
+  };
+
+  toggleLoading = () => {
+    this.setState({ loading: !this.state.loading });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -16,13 +47,13 @@ class App extends Component {
         <Routes>
           <Route path='/' element={
             <React.Fragment>
-              <Form />
-              <MoviesList />
+              <Form hoistInputFromMoviesForm={this.hoistInputFromMoviesForm} />
+              {(this.state.resultsFromServer.length > 0) ? <MyMoviesList results={this.state.resultsFromServer} /> : ''}
             </React.Fragment>
           } />
           <Route path='about' element={<About />} />
           <Route path='aboutMichellePannosch' element={<AboutMichellePannosch />} />
-          <Route path='MoviesList' element={<MoviesList />} />
+          <Route path='MoviesList' element={<MoviesList results={this.state.resultsFromServer} toggleLoading={this.toggleLoading} />} />
         </Routes>
         <Footer />
       </React.Fragment>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import '../style/Form.css';
 
-class Form extends Component {
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieSearch: '',
+      searchResults: [],
+    };
+  }
+  handleChange = e => {
+    const userInput = e.target.value;
+    this.setState({ movieSearch: userInput });
+    // console.log('here is the userInput from state', userInput);
+    this.renderUserInput(userInput);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ movieSearch: e.target.moviesinput.value });
+    console.log(
+      'we are in From element and here is e.target.moviesinput.value',
+      e.target.moviesinput.value
+    );
+    this.props.hoistInputFromMoviesForm(e.target.moviesinput.value);
+  };
+
+  renderUserInput = userInput => {
+    const userInputRender2Page = document.getElementById('user-input');
+    userInputRender2Page.innerText = userInput;
+    if (!userInput)
+      userInputRender2Page.innerText = 'the search field is empty right now';
+  };
   render() {
     return (
-      <div>Form</div>
+      <Container fluid='md' maxwidth='sm'>
+        <h3>Form</h3>
+        <h3>
+          and here is just a reminder of what you typed and what we are
+          currently searching for:
+        </h3>
+        <p id='user-input'>nothing yet</p>
+        <form id='movies-search-form' onSubmit={this.handleSubmit}>
+          <fieldset>
+            <legend>
+              Please type a search term in the input field to look for movies
+            </legend>
+            {/* <label htmlFor='movies-input'>enter your movie search here</label>
+            <br /> */}
+            <input
+              type='text'
+              id='movies-input'
+              name='moviesinput'
+              onChange={this.handleChange}
+            ></input>
+            <br />
+            <button type='submit'>click here to search for movies</button>
+          </fieldset>
+        </form>
+      </Container>
     );
   }
 }
