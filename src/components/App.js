9a96e5f43be7cb74b2234b36a16a0f985086a987
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-14 20:37:26 -0800

    nav bar wired up with links and App.js with corresponding react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import '../style/App.css';
 import Header from './Header.js';
 import Form from './Form.js';
 import Footer from './Footer.js';
 import About from './About.js';
 import AboutMichellePannosch from './AboutMichellePannosch.js';
-import MovieList from './MovieList.js';
+import MoviesList from './MoviesList.js';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header />
-        <Form />
-        <About />
-        <AboutMichellePannosch />
-        <MovieList />
-        Hello from App here is proof of life
+        <Routes>
+          <Route path='/' element={
+            <React.Fragment>
+              <Form />
+              <MoviesList />
+            </React.Fragment>
+          } />
+          <Route path='about' element={<About />} />
+          <Route path='aboutMichellePannosch' element={<AboutMichellePannosch />} />
+          <Route path='MoviesList' element={<MoviesList />} />
+        </Routes>
         <Footer />
       </React.Fragment>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import '../style/Header.css';
 
 class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <Container fluid='md' maxwidt='sm'>
+        <nav>
+          <h1>My All Times Favorite Movies LIST</h1>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='about'>about My Favorite Movies LIST APP</Link>
+            </li>
+            <li>
+              <Link to='aboutMichellePannosch'>About Michelle Pannosch</Link>
+            </li>
+            <li>
+              <Link to='MoviesList'>My Favorite Movies LIST</Link>
+            </li>
+          </ul>
+        </nav>
+      </Container>
     );
   }
 }
-
 export default Header;
