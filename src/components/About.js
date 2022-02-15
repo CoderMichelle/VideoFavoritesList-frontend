14c72de825637b96e8b1ccfbd676d13e8922338c
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-14 22:43:04 -0800

    adds flipcard about me, center text about, change css for Header nav bar, tested - works

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
 
 class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <Container fluid='md' maxwidth='sm'>
+        <h1>About</h1>
+      </Container >
     );
   }
 }
